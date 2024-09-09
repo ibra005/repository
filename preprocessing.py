fd41270fb1e96278b8d08ff5a6a9679bee7e3031
ibra005 <plumbumso4@gmail.com>
2024-09-09 18:06:44 +0300

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,2 +1,14 @@
-def pprocess(df: pandas.DataFrame):
-    pass
+def pprocess(df):
+    print('\033[34mВыведем данные из датасета\033[0m')
+    display(df)
+   
+    print('\033[34mВыведем общую информацию о датасете\033[0m')
+    print()
+    df.info()
+    
+    print()
+    print('\033[34mВыведем описание датасета\033[0m')
+    display(df.describe())
+    
+    print('\033[34mВыведем дубли\033[0m')
+    display(df[df.duplicated()])
